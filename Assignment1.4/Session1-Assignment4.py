3a19cb49ad578422731b120107475f94ed0be524
sasmita-sabat <35617622+sasmita-sabat@users.noreply.github.com>
2018-02-04 20:14:12 +0530

    Add files via upload
    Assignment4

---
+++ Assignment1.4/Session1-Assignment4.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[15]:
+
+
+#To find out the volume of the sphere using the below formula
+Dia=input("Enter the Diameter of the Sphere ")
+r=int(Dia)/2
+#22/7 denotes Pi
+V=str(((4/3) * 22/7) *r **3)
+print("The Volume of the sphere is "+V)
+
